38b863b434c1d2ebbcd8f5254ef1ee3ddfbc47bf
Forest Katsch <forestcgk@gmail.com>
2020-10-28 16:40:38 -0500

    Initial Commit

---
+++ Sonar/SonarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonarApp.swift
+//  Sonar
+//
+//  Created by Forest Katsch on 10/28/20.
+//
+
+import SwiftUI
+
+@main
+struct SonarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
